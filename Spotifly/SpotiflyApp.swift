d3e40af0fb12cffe71cc56a81e04292dbf7372e5
Ralph von der Heyden <ralph@rvdh.de>
2025-12-30 17:33:14 +0100

    Initial commit

---
+++ Spotifly/SpotiflyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpotiflyApp.swift
+//  Spotifly
+//
+//  Created by Ralph von der Heyden on 30.12.25.
+//
+
+import SwiftUI
+
+@main
+struct SpotiflyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SpotiflyUITests/SpotiflyUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  SpotiflyUITestsLaunchTests.swift
+//  SpotiflyUITests
+//
+//  Created by Ralph von der Heyden on 30.12.25.
+//
+
+import XCTest
+
+final class SpotiflyUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
