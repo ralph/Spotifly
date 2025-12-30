116fe301c8cac363e662b81a3b536534d224ae6b
Ralph von der Heyden <ralph@rvdh.de>
2025-12-30 17:33:14 +0100

    Initial commit

---
+++ Spotifly/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Spotifly
+//
+//  Created by Ralph von der Heyden on 30.12.25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SpotiflyTests/SpotiflyTests.swift
@@ -0,0 +1,16 @@
+//
+//  SpotiflyTests.swift
+//  SpotiflyTests
+//
+//  Created by Ralph von der Heyden on 30.12.25.
+//
+
+import Testing
+
+struct SpotiflyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
